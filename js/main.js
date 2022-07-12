f4ed0376be76c7b3b44440ae6f4b372371093f05
homerioni <homerion13@gmail.com>
2022-07-12 13:12:07 +0300

    QA

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 'use strict'
 
 $(document).ready(function () {
+
+    // Q&A
+    $('.qa__question').click(function () {
+        $(this).toggleClass('open')
+            .parent().find('.qa__answer').slideToggle();
+    });
+
 });
